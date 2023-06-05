280abb4afb97254f8db5d9e259ccff4b6c832745
Roktim Kamal Senapoty <roktimsenapoty2@gmail.com>
2023-06-05 20:55:18 +0530

    homepage change

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
             className="button button--secondary button--lg"
             to="/docs/intro"
           >
-            Lets Learn ⏱️
+            Lets Learn Together ⏱️
           </Link>
         </div>
       </div>
